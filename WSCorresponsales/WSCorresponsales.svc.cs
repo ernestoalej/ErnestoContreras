a927fcf0bd03fd4088ebd333ab75c58dc8bdb09a
FREDCOM-PC <FREDCOM-PC@FREDCOM-PC>
2018-03-07 23:48:52 -0400

    Crear Servicio Base obtenerCorresponsales()

---
+++ WSCorresponsales/WSCorresponsales.svc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WSCorresponsales
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WSCorresponsalesImpl : IWSCorresponsales
+    {
+        string cnnString = "";
+
+        public WSCorresponsalesImpl()
+        {
+            cnnString = System.Configuration.ConfigurationManager.ConnectionStrings["cnn"].ToString();
+        }
+
+        public Corresponsal obtenerCorresponsales()
+        {
+            string sql = "SELECT COR_CORRESPONSAL_ID, COR_NOMBRE FROM CORRESPONSALES;";
+
+            using (SqlConnection cnn = new SqlConnection(cnnString))
+            {
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand(sql, cnn);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while(dr.Read())
+                {
+                    Corresponsal corr = new Corresponsal() {
+
+                        id = Convert.ToInt32(dr["COR_CORRESPONSAL_ID"]),
+                        nombre = dr["COR_NOMBRE"].ToString()
+
+                    };
+
+                    return corr;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ WSCorresponsales/csCorresponsales.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WSCorresponsales
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IWSCorresponsales
+    {
+        [OperationContract]
+        Corresponsal obtenerCorresponsales();
+        
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    [DataContract]
+    public class Corresponsal
+    {
+        [DataMember]
+        public int id { get; set; }
+
+        [DataMember]
+        public string nombre  { get; set; }
+    }
+
+}
